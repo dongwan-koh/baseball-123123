49899ac0c52e668e3cdeead1ddd35be835042d17
dw.koh <dw0402.koh@samsung.com>
2024-04-29 10:35:00 +0900

    [feature] 27

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,2 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GameTest {
+
+    @Test
+    void createGame() {
+        Game game = new Game();
+        assertNotNull(game);
+    }
+
+    @Test
+    void throwExceptionWhenInputIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Game game = new Game();
+            game.guess(null);
+        });
+    }
 }
